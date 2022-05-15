16e42ccb740fd999ff991087c080819e3eaafef2
DiegoSousaRodrigues2 <rm87910@fiap.com.br>
2022-05-14 21:22:25 -0300

    finalizado

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import speech_recognition as sr
 import pyttsx3
 from datetime import datetime
 from unidecode import unidecode
+import random
+import wikipedia
 
 recon = sr.Recognizer()
 engine = pyttsx3.init()
+wikipedia.set_lang("pt")
 
 
 def talk(text):
@@ -29,6 +32,7 @@ def aguardando_sexta_feira():
         try:
             while True:
                 print("pode falar")
+                recon.adjust_for_ambient_noise(source, duration=0.3)
                 audio = recon.listen(source)
                 command = recon.recognize_google(audio, language='pt')
                 command = command.lower()
@@ -57,12 +61,12 @@ def cadastrar_aquivo(compromisso):
         arquivo = open('agenda.txt', 'w')
         arquivo.write(unidecode(compromisso, '\n'))
         arquivo.close()
-        print("cadastrado")
+        talk("compromisso salvo")
     else:
         arquivo = open('agenda.txt', 'a')
         arquivo.write(unidecode(compromisso, '\n'))
         arquivo.close()
-        print("incrementado")
+        talk("compromisso adicionado")
 
 
 def ler_arquivo():
@@ -74,12 +78,116 @@ def ler_arquivo():
     return texto_para_falar
 
 
+def advinhe_o_numero():
+    try:
+        command = ""
+        with sr.Microphone() as source:
+            talk("Fale um numero de 1 a 10")
+            audio = recon.listen(source)
+            command = recon.recognize_google(audio, language='pt')
+            command = command.lower()
+            validar_numero(command)
+    except:
+        pass
+
+
+def validar_numero(numero):
+    try:
+        numero = int(numero)
+        if type(numero) == int and 0 < numero < 11:
+            a = random.randint(0, 10)
+            if numero == a:
+                talk("Parabéns você acertou")
+            else:
+                talk("Sinto muito você errou")
+    except:
+        talk('Não foi informado um numero valido')
+
+
+def pesquisar_wikipedia():
+    try:
+        command = ""
+        with sr.Microphone() as source:
+            talk("Me fale um topico para pesquisar")
+            audio = recon.listen(source)
+            command = recon.recognize_google(audio, language='pt')
+            command = command.lower()
+            if command != "":
+                return command
+    except:
+        pass
+
+
+def limpar_agenda():
+    arquivo = open('agenda.txt', 'w')
+    arquivo.write('')
+    arquivo.close()
+    talk('agenda limpa')
+
+
+def listar_compromisso_dicionario():
+    key = []
+    linha_key = []
+    for a in range(1, len(ler_arquivo())+1):
+        key.append(a)
+        linha_key.append('linha' + str(a))
+
+    value = ler_arquivo()
+    key_value = dict(zip(key, value))
+    linha_key_value = dict(zip(linha_key, value))
+    talk(linha_key_value)
+    apagar_compromisso(key_value, len(value))
+
+
+def apagar_compromisso(lista, quant_linha):
+    try:
+        command = ""
+        with sr.Microphone() as source:
+            talk("Por favor diga o numero da linha que deseja apagar")
+            audio = recon.listen(source)
+            command = recon.recognize_google(audio, language='pt')
+            command = command.lower()
+            if command != "":
+                valid_numero_linha(command, quant_linha, lista)
+
+    except:
+        pass
+
+
+def valid_numero_linha(command, quant_linha, lista):
+    try:
+        command = int(command)
+        if type(command) == int and 0 < command < quant_linha+1:
+            apagar_linha(command, lista)
+        else:
+            talk("Linha não existe")
+    except:
+        talk('Não foi informado um numero valido')
+
+
+def apagar_linha(linha_apagar, lista):
+    print(lista)
+    del lista[linha_apagar]
+    print(lista)
+
+    nova_lista = []
+
+    for a, b in lista.items():
+        nova_lista.append(b + '\n')
+    arquivo = open('agenda.txt', 'w')
+    for a in nova_lista:
+        arquivo.write(a)
+    arquivo.close()
+    talk("compromisso apagado")
+
+
 def main():
     try:
+        command = ''
         while True:
             command = comando()
             print(command)
-            if command in 'data':
+            if command in 'data' or command == 'que dia é hoje' or command in 'dia':
                 d1 = datetime.today().strftime('%d-%m-%Y')
                 talk(d1)
                 return
@@ -90,8 +198,31 @@ def main():
                 texto_para_falar = ler_arquivo()
                 talk(texto_para_falar)
                 return
+            elif command in 'hora' or command == 'que horas são':
+                d1 = datetime.today().strftime('%H-%M')
+                talk(d1)
+                return
+            elif 'Cara' in command or 'coroa' in command or command == 'cara ou coroa':
+                if random.randint(1, 2) == 1:
+                    talk("Saiu Cara")
+                else:
+                    talk("Saiu Coroa")
+                return
             elif 'solteira' in command or command == 'você esta solteira':
                 talk('Eu estou em um relacionamento com o wi-fi')
+                return
+            elif 'wikipédia' in command or command == 'pesquisar no wikipedia':
+                pesquisa = wikipedia.summary(pesquisar_wikipedia())[0:150]
+                talk(pesquisa)
+                return
+            elif 'limpar agenda' in command:
+                limpar_agenda()
+                return
+            elif 'apagar compromisso' in command or command == 'apagar compromisso':
+                listar_compromisso_dicionario()
+                return
+            elif 'advinha' in command or command == 'advinha o numero' or command == 'o numero':
+                advinhe_o_numero()
                 return
             elif 'encontro' in command:
                 talk('desculpe, estou com dor de cabeca')
